1e8501b5ce05d634050c02f2e02a994e8fe2e2fc
nmlkj123 <51207216+nmlkj123@users.noreply.github.com>
2022-02-22 17:15:55 +0900

    Update smTraffic.py

--- smTraffic.py
+++ smTraffic.py
@@ -39,9 +39,9 @@ def wheel_element(element, deltaY = 120, offsetX = 0, offsetY = 0):
 #borwser = webdriver.Chrome("./chromedriver.exe")
 
 print("키워드입력:",end="")
-keyword = input("나도코딩")
+keyword = "나도코딩"
 print("아이템아이디:",end="")
-itemId = input("82520022261")
+itemId = "82520022261"
 
 
 browser = webdriver.Chrome()
